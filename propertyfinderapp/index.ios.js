b979ef7b9ea2110099ccbea72fc371db56ea143c
lisaztruong <lisaztruong@gmail.com>
2017-04-02 15:11:25 -0700

    working on searchpage component

--- propertyfinderapp/index.ios.js
+++ propertyfinderapp/index.ios.js
@@ -3,7 +3,7 @@
  * https://github.com/facebook/react-native
  * @flow
  */
- 
+
 'use strict'
 
 import React, { Component } from 'react';
@@ -11,34 +11,35 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  NavigatorIOS
 } from 'react-native';
 
-export default class propertyfinderapp extends Component {
+var SearchPage = require('./SearchPage');
+
+class propertyfinderapp extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: SearchPage,
+        }}/>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+
   },
   welcome: {
     fontSize: 20,
